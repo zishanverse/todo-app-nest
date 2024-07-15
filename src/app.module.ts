150f40f05dd12559e876fc550bf18948f244dc88
zishanverse <zishanverse@gmail.com>
2024-07-15 23:29:54 +0530

    init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from './database/database.module';
+import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [DatabaseModule, TodoModule, AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
