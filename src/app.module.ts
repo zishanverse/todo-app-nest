05dd076f7ab6ce12ee9a414ec981f9ba42933230
zishanverse <zishanverse@gmail.com>
2024-07-16 18:33:52 +0530

    bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
